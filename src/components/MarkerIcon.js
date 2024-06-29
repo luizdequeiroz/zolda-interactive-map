869bec7ea7171fe958af9ec83b28a8e96bd807da
Luiz de Queiroz <oluizdequeiroz@gmail.com>
2024-06-29 02:38:18 -0300

    adjust marker renderizations

--- src/components/MarkerIcon.js
+++ src/components/MarkerIcon.js
@@ -3,17 +3,17 @@ import L from 'leaflet';
 
 const firstMarkerIcon = new L.Icon({
   iconUrl: `${process.env.PUBLIC_URL}/img/first-marker-icon.png`,
-  iconSize: [25, 41], // Ajuste conforme necessário
-  iconAnchor: [12, 41], // Ajuste conforme necessário
-  popupAnchor: [1, -34], // Ajuste conforme necessário
+  iconSize: [43, 44], // Ajuste conforme necessário
+  iconAnchor: [22, 41], // Ajuste conforme necessário
+  popupAnchor: [4.3, -44], // Ajuste conforme necessário
   shadowSize: [41, 41] // Ajuste conforme necessário
 });
 
 const subsequentMarkerIcon = new L.Icon({
   iconUrl: `${process.env.PUBLIC_URL}/img/subsequent-marker-icon.png`,
-  iconSize: [25, 41], // Ajuste conforme necessário
-  iconAnchor: [12, 41], // Ajuste conforme necessário
-  popupAnchor: [1, -34], // Ajuste conforme necessário
+  iconSize: [25, 35], // Ajuste conforme necessário
+  iconAnchor: [12, 35], // Ajuste conforme necessário
+  popupAnchor: [2.5, -35], // Ajuste conforme necessário
   shadowSize: [41, 41] // Ajuste conforme necessário
 });
 
