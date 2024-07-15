e2183f58bc72f730a0650f48ce9bf22fbe24294c
Luiz de Queiroz <oluizdequeiroz@gmail.com>
2024-07-15 00:16:55 -0300

    hide clear dice roller button when not rolled dices

--- src/components/DiceRoller/DiceControls.js
+++ src/components/DiceRoller/DiceControls.js
@@ -1,15 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function DiceControls({ rollDice, resetDice }) {
+  const [rolledDices, setRolledDices] = useState(false)
+
+  const roll = (notation) => {
+    rollDice(notation);
+    setRolledDices(true);
+  }
+
+  const clear = () => {
+    resetDice();
+    setRolledDices(false);
+  }
+
   return (
     <div className="dice-controls">
-      <button className="dice-button d4" onClick={() => rollDice("1d4")}></button>
-      <button className="dice-button d6" onClick={() => rollDice("1d6")}></button>
-      <button className="dice-button d8" onClick={() => rollDice("1d8")}></button>
-      <button className="dice-button d10" onClick={() => rollDice("1d10")}></button>
-      <button className="dice-button d12" onClick={() => rollDice("1d12")}></button>
-      <button className="dice-button d20" onClick={() => rollDice("1d20")}></button>
-      <button onClick={resetDice}>Limpar</button>
+      {rolledDices && <button onClick={clear}>Limpar</button>}
+      <button className="dice-button d4" onClick={() => roll("1d4")}></button>
+      <button className="dice-button d6" onClick={() => roll("1d6")}></button>
+      <button className="dice-button d8" onClick={() => roll("1d8")}></button>
+      <button className="dice-button d10" onClick={() => roll("1d10")}></button>
+      <button className="dice-button d12" onClick={() => roll("1d12")}></button>
+      <button className="dice-button d20" onClick={() => roll("1d20")}></button>
     </div>
   );
 }
